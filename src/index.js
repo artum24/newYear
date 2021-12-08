aaccc6eb5864e037618b0a15267dd3b020367613
artum24 <artyr.polianskij@gmail.com>
2021-12-08 21:54:49 +0200

    fix styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './styles/index.scss';
+import './styles/main.scss';
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
 import {Provider} from "react-redux";
